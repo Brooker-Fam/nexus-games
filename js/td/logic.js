bebcd3ed2e0e77ecf74d80c55c66d6dd366a54fd
Matt Brooker <matt@posthog.com>
2026-04-15 11:53:01 -0400

    fix: remove undefined TD_CONFIG.tooltips.waveStart access in startWave

--- js/td/logic.js
+++ js/td/logic.js
@@ -137,7 +137,6 @@ function setSpeed(s, btn){
 
 function startWave(){
   if(state.waveActive || state.gameOver) return;
-  var hint = TD_CONFIG.tooltips.waveStart;
   state.wave++;
   const count = TD_CONFIG.baseEnemyCount + state.wave * TD_CONFIG.enemyCountScaling;
   state.waveActive = true;
